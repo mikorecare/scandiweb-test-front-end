a86d46a29460652c6308392889d38c877d259fd6
SvenSven <mikorecare@gmail.com>
2025-05-03 12:07:32 +0800

    CHORE: removed abstract

--- shop-app/src/models/products/product.abstract.class.ts
+++ shop-app/src/models/products/product.abstract.class.ts
@@ -2,14 +2,16 @@ import { AttributeSet } from "../attributes/attribute-set.class";
 import { Price } from "../prices/price.class";
 import { Category } from "./category.enum";
 
-export abstract class Product {
-  public readonly id!: string;
-  public readonly name!: string;
-  public readonly inStock!: boolean;
-  public readonly gallery!: string[];
-  public readonly description!: string;
-  public abstract readonly category: Category;
-  public readonly attributes!: AttributeSet[];
-  public readonly prices!: Price[];
-  public readonly brand!: string;
+export class Product {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly inStock: boolean,
+    public readonly gallery: string[],
+    public readonly description: string,
+    public readonly category: Category,
+    public readonly attributes: AttributeSet[],
+    public readonly prices: Price[],
+    public readonly brand: string
+  ) {}
 }
